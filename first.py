296c0f4910fbefb9820a0994850dc4621c01332d
ARPITASHALINI <54719517+ARPITASHALINI@users.noreply.github.com>
2019-09-05 15:58:57 -0400

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+#This is demo script
+#Arpita Shalini
+#We will calculate the area amd perimeter of a circle
+
+pi = 3.14
+r = 2.0
+area = pi *(r**2)
+perim = 2.0*pi*r
+print"Given r: ",r
+print "Area is: ",area
+print "Perimeter is: ",perim
